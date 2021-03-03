926ce4bb8af8181a772cc64458ec3e14e39448aa
xtineeyc <64254608+xtineeyc@users.noreply.github.com>
2021-03-03 17:53:11 +0800

    Update 1.minerva_score_establishment.R

--- 1.minerva_score_establishment.R
+++ 1.minerva_score_establishment.R
@@ -27,3 +27,22 @@ for(i in 1:171){
 }
 mnvgenes$score = score
 write.csv(mnvgenes, file="minerva_scores.csv", row.names = T,  quote = F)
+
+
+#### prognostic biomarkers
+prog_cox <-list()
+for (i in 12:ncol(data)){
+  prog_uni<-coxph(Surv(DFS,DFS_status)~data[,i]+data[,1],data=data, na.action=na.exclude)
+  prog_uni<-as.data.frame(as.matrix(coef(summary(prog_uni))))
+  prog_uni$feature<-colnames(data)[[i]]
+  prog_cox[[i]]<-prog_uni
+}
+pcox_result <- do.call(rbind,prog_cox)
+
+prog_idx=c()
+for(i in 1:dim(pcox_result)[1]){
+  if(i%%2==0){prog_idx=c(prog_idx,i)} 
+}
+new_pcox_result=pcox_result[-prog_idx,]
+prog_mark <- new_pcox_result[which(as.numeric(as.character(new_pcox_result[,5]))<=0.05), ]
+write.csv(prog_mark, file="cox.prognostic.filter0.05.csv", row.names = F, quote = F)
